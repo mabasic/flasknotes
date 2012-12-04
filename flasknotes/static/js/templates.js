7c37bfe2d045f88e67778e1304b8b6b4fc00b83a
Mario Basic <mario.basic@damex.hr>
2012-12-04 13:29:27 +0100

    Removed compiled jade templates file from repository

--- flasknotes/static/js/templates.js
+++ flasknotes/static/js/templates.js
@@ -1,9 +1,16 @@
-var jade_blog = function anonymous(locals, attrs, escape, rethrow, merge) {
+var jade_userbar = function anonymous(locals, attrs, escape, rethrow, merge) {
 attrs = attrs || jade.attrs; escape = escape || jade.escape; rethrow = rethrow || jade.rethrow; merge = merge || jade.merge;
 var buf = [];
 with (locals || {}) {
 var interp;
-buf.push('<div class="row-fluid"><div class="well span3"><h4>Day 1</h4><blockquote><p> <ul><li>Created application structure</li></ul></p><small>Author: <cite title="Mario Bašić">Mario Bašić</cite>, 27.11.2012</small></blockquote></div><div class="well span3"><h4>Day 2</h4><blockquote><p><ul><li>Used Bootstrap to define the application design. </li><li>Reordered python files to improve import organization.</li><li>Defined application functionality</li></ul></p><small>Author: <cite title="Mario Bašić">Mario Bašić</cite>, 28.11.2012</small></blockquote></div><div class="well span3"><h4>Day 3</h4><blockquote><p><ul><li>Using backbone.js created log in / log out bar</li><li>Created server api for handling session </li><li>Dropped marionette.js because it lacks documentation</li></ul></p><small>Author: <cite title="Mario Bašić">Mario Bašić</cite>, 29.11.2012</small></blockquote></div><div class="well span3"><h4>Day 4</h4><blockquote><p><ul><li>Created a backbone router to handle navigation</li><li>Created a view for showing blog posts :)</li><li>Created a view for showing registration form</li><li>Updated site with icons</li><li>Using sessionStorage for handling sessions</li><li>Users can now register; need to implement md5</li></ul></p><small>Author: <cite title="Mario Bašić">Mario Bašić</cite>, 30.11.2012</small></blockquote></div></div><!-- div.row-fluid<div class="well span3"><h4>Day 4</h4><blockquote><p><ul><li>Created a backbone router to handle navigation</li><li>Created a view for showing blog posts :)</li></ul></p><small>Author: <cite title="Mario Bašić">Mario Bašić</cite>, 30.11.2012</small></blockquote></div><div class="well span3"><h4>Day 4</h4><blockquote><p><ul><li>Created a backbone router to handle navigation</li><li>Created a view for showing blog posts :)</li></ul></p><small>Author: <cite title="Mario Bašić">Mario Bašić</cite>, 30.11.2012</small></blockquote></div>-->');
+if ( session_active == null )
+{
+buf.push('<input type="text" placeholder="Username" style="margin-right: 4px" name="username" class="span2"/><input type="password" placeholder="Password" style="margin-right: 4px" name="password" class="span2"/><button type="button" class="btn login"><i class="icon-user"></i> Log in</button>');
+}
+else
+{
+buf.push('<button type="button" class="btn logout"><i class="icon-user"> </i> Log out</button>');
+}
 }
 return buf.join("");
 }
@@ -25,19 +32,12 @@ buf.push('<form class="form-horizontal"><div id="formUsername" class="control-gr
 }
 return buf.join("");
 }
-var jade_userbar = function anonymous(locals, attrs, escape, rethrow, merge) {
+var jade_blog = function anonymous(locals, attrs, escape, rethrow, merge) {
 attrs = attrs || jade.attrs; escape = escape || jade.escape; rethrow = rethrow || jade.rethrow; merge = merge || jade.merge;
 var buf = [];
 with (locals || {}) {
 var interp;
-if ( session_active == null )
-{
-buf.push('<input type="text" placeholder="Username" style="margin-right: 4px" name="username" class="span2"/><input type="password" placeholder="Password" style="margin-right: 4px" name="password" class="span2"/><button type="button" class="btn login"><i class="icon-user"></i> Log in</button>');
-}
-else
-{
-buf.push('<button type="button" class="btn logout"><i class="icon-user"> </i> Log out</button>');
-}
+buf.push('<div class="row-fluid"><div class="well span3"><h4>Day 1</h4><blockquote><p> <ul><li>Created application structure</li></ul></p><small>Author: <cite title="Mario Bašić">Mario Bašić</cite>, 27.11.2012</small></blockquote></div><div class="well span3"><h4>Day 2</h4><blockquote><p><ul><li>Used Bootstrap to define the application design. </li><li>Reordered python files to improve import organization.</li><li>Defined application functionality</li></ul></p><small>Author: <cite title="Mario Bašić">Mario Bašić</cite>, 28.11.2012</small></blockquote></div><div class="well span3"><h4>Day 3</h4><blockquote><p><ul><li>Using backbone.js created log in / log out bar</li><li>Created server api for handling session </li><li>Dropped marionette.js because it lacks documentation</li></ul></p><small>Author: <cite title="Mario Bašić">Mario Bašić</cite>, 29.11.2012</small></blockquote></div><div class="well span3"><h4>Day 4</h4><blockquote><p><ul><li>Created a backbone router to handle navigation</li><li>Created a view for showing blog posts :)</li><li>Created a view for showing registration form</li><li>Updated site with icons</li><li>Using sessionStorage for handling sessions</li><li>Users can now register; need to implement md5</li></ul></p><small>Author: <cite title="Mario Bašić">Mario Bašić</cite>, 30.11.2012</small></blockquote></div></div><!-- div.row-fluid<div class="well span3"><h4>Day 4</h4><blockquote><p><ul><li>Created a backbone router to handle navigation</li><li>Created a view for showing blog posts :)</li></ul></p><small>Author: <cite title="Mario Bašić">Mario Bašić</cite>, 30.11.2012</small></blockquote></div><div class="well span3"><h4>Day 4</h4><blockquote><p><ul><li>Created a backbone router to handle navigation</li><li>Created a view for showing blog posts :)</li></ul></p><small>Author: <cite title="Mario Bašić">Mario Bašić</cite>, 30.11.2012</small></blockquote></div>-->');
 }
 return buf.join("");
 }
