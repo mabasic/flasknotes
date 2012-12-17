9be88070159aa87edd6cc7f796d9d1d79e1d53c1
Mario Basic <mario.basic@damex.hr>
2012-12-17 12:57:13 +0100

    Implemented database history using mixins for class inheritance.

--- flasknotes/models/models.py
+++ flasknotes/models/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship, backref
 from datetime import datetime
+from sqlalchemy.ext.declarative import declared_attr
 
 from flasknotes import Base
 
@@ -25,15 +26,25 @@ class User(Base):
     def __repr__(self):
         return "<User('%s','%s')>" % (self.username, self.password)
 
-class Note(Base):
-    __tablename__ = "notes"
-
+# Class for storing common attributes of multiple tables
+class NoteMix(object):
     idNote = Column(Integer, primary_key=True)
     title = Column(String(50))
     text = Column(String(50))
 
-    user_id = Column(Integer, ForeignKey("users.idUser"))
-    user = relationship("User", backref=backref("notes", order_by=idNote))
+    # if you are using a relationship or foreign key , you must use @declared_attr
+    # and you must use the syntax below to return that relationship or foreign key
+    @declared_attr
+    def user_id(cls):
+        return Column(Integer, ForeignKey("users.idUser"))
+
+    # cls.__name__ = inherited table name
+    @declared_attr
+    def user(cls):
+        return relationship("User", backref=backref(cls.__name__), primaryjoin="%s.user_id==User.idUser"%cls.__name__)
+
+class Note(NoteMix, Base):
+    __tablename__ = "notes"
 
     def __init__(self, title, text, user_id):
         self.title = title
@@ -50,24 +61,13 @@ class Note(Base):
             'text': self.text
             }
 
-    __mapper_args__ = {
-        'polymorphic_identity':'notes',
-    }
-
-class NoteHistory(Base):
+class NoteHistory(NoteMix, Base):
     __tablename__ = "notes_history"
 
-    idNote = Column(Integer, primary_key=True)
-    title = Column(String(50))
-    text = Column(String(50))
-
-    user_id = Column(Integer, ForeignKey("users.idUser"))
-    user = relationship("User", primaryjoin="NoteHistory.user_id==User.idUser")
-
-    user_id_history = Column(Integer, ForeignKey("users.idUser")) # korisnik koji je promijenio podatak
+    user_id_history = Column(Integer, ForeignKey("users.idUser"))
     user_history = relationship("User", primaryjoin="NoteHistory.user_id_history==User.idUser")
 
-    datetime_change = Column(DateTime, default=datetime.now) # svaki puta kada se redak kreira, zapisuje se vrijeme u ovo polje po defaultu
+    datetime_change = Column(DateTime, default=datetime.now)
 
     def __init__(self, title, text, user_id, user_id_history):
         self.title = title
@@ -77,4 +77,3 @@ class NoteHistory(Base):
 
     def __repr__(self):
         return "<Note('%s','%s'.'%s')>" % (self.title, self.text, self.user_id, self.user_id_history)
-        
